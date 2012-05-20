1b4475a3bbf1d702b5d5ac80d76066047855e5e9
Neo Lehtola <pklehtol@cs.helsinki.fi>
2012-05-20 20:55:21 +0300

    tekstikälin raakile

--- 15-puzzle/src/kayttoliittyma/Main.java
+++ 15-puzzle/src/kayttoliittyma/Main.java
@@ -1,13 +1,19 @@
 
 package kayttoliittyma;
 
-import javax.swing.SwingUtilities;
+//import java.util.Scanner;
+import sovelluslogiikka.PelilaudanKayttaja;
+//import javax.swing.SwingUtilities;
 
 public class Main {
     
     public static void main(String[] args) {
-        GUI gui = new GUI();
-        SwingUtilities.invokeLater(gui);
+//        GUI gui = new GUI();
+//        SwingUtilities.invokeLater(gui);
+
+        Tekstikayttoliittyma t = new Tekstikayttoliittyma(new PelilaudanKayttaja(4, 4, 10000));
+        t.tulostaPelilauta();
+        
     }
     
 }
--- 15-puzzle/src/kayttoliittyma/Tekstikayttoliittyma.java
+++ 15-puzzle/src/kayttoliittyma/Tekstikayttoliittyma.java
@@ -1,13 +1,43 @@
 
 package kayttoliittyma;
 
+import java.util.Scanner;
 import sovelluslogiikka.PelilaudanKayttaja;
 
 public class Tekstikayttoliittyma {
     
-    public static void main(String[] args) {
-
-
+    private PelilaudanKayttaja kayttaja;
+    
+    
+    public Tekstikayttoliittyma(PelilaudanKayttaja kayttaja) {
+        this.kayttaja = kayttaja;
+        
     }
+    // missä vaiheessa tämä valinta suoritetaan, jos laudan koko 
+    // pitää määrittää jo konstruktorin parametrin parametreina?
+    public void valitseLaudanKoko(Scanner lukija) throws Exception {
+        System.out.println("Tervetuloa pelaamaan 15-puzzlea");
+        System.out.println("Voit valita pelilaudan leveyden ja korkeuden");
+        System.out.println("väliltä " + kayttaja.getSIVUNALARAJA() + " - " + kayttaja.getSIVUNYLARAJA());
+        
+    }
+    
+    public void tulostaPelilauta() {
+        for (int i = 0; i < kayttaja.getKorkeus(); i++) {
+            for (int j = 0; j < kayttaja.getLeveys(); j++) {
+                int nro = kayttaja.getNappula(i, j).getTunniste();
+                if (nro == -1) {
+                    System.out.print("   ");
+                    continue;
+                }
+                if (nro < 10) {
+                    System.out.print(" ");
+                }
+                System.out.print(nro + " ");
+            }
+            System.out.println("");
+        }
+    }
+
     
 }
