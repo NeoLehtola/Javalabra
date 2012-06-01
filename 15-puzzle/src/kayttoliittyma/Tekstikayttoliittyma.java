8f094babd21feb6e68e5786ccf5a98fce05eece1
Neo Lehtola <pklehtol@cs.helsinki.fi>
2012-06-01 23:07:50 +0300

    suunnitteludokumenttiin muutama sana lopullisesta toteutuksesta. sekvenssikaaviot olivat ajan tasalla

--- 15-puzzle/src/kayttoliittyma/Tekstikayttoliittyma.java
+++ 15-puzzle/src/kayttoliittyma/Tekstikayttoliittyma.java
@@ -2,7 +2,6 @@ package kayttoliittyma;
 
 import java.util.Scanner;
 import sovelluslogiikka.PelinLataaja;
-import sovelluslogiikka.PelinTallentaja;
 import sovelluslogiikka.Pelitapahtuma;
 
 public class Tekstikayttoliittyma {
